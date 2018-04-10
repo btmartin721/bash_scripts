9f83413f49794d5dd0b6be5002bc68f8c8c2e121
btmartin721 <btm002@email.uark.edu>
2018-04-10 16:43:04 -0500

    Initial commit

--- submitSTR.sh
+++ submitSTR.sh
@@ -7,6 +7,7 @@ INFILE=$1
 KSTART=$2
 KEND=$3
 TEST_FLAG=$4
+
 # check if there are at least three command-line arguments
 # test_flag allows the user to view the modified .pbs files without submitting to queue
 # If you want to use TEST_FLAG, just type any string as the fourth command-line argument
@@ -22,9 +23,11 @@ if [ ! -f $INFILE ]; then
 	exit 1
 fi
 
+
 # TEST_FLAG is the fourth command-line argument
 # if TEST_FLAG is defined, the script will print to STDOUT instead of submitting to queue		
 if [ $TEST_FLAG ]; then
+
 
 # Submits structure runs with K=$2 to K=$3
 # The sed command replaces an @ symbol in the .pbs script
